1526e556297f210074819226725fc49f81d20a4a
Lucas Vogel <contact@vogelino.com>
2024-08-25 12:35:13 -0400

    move re-gards to here

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+import vercel from "@astrojs/vercel/static";
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx(), tailwind()],
+  output: "static",
+  adapter: vercel()
+});
